5240639c3c0e22e29ed4450fea721f0bd390658a
CByamamoto <51428471+CByamamoto@users.noreply.github.com>
2019-07-12 17:50:00 +0900

    MST02011(品番マスタ一括修正)とMST01011(取引先マスタ一括修正)の画面を作成

---
+++ Application/Windows/Views/MST/MST01011.xaml.cs
@@ -0,0 +1,420 @@
+﻿using KyoeiSystem.Framework.Common;
+using KyoeiSystem.Framework.Core;
+using KyoeiSystem.Framework.Windows.ViewBase;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+using System.Windows.Input;
+
+namespace KyoeiSystem.Application.Windows.Views
+{
+    using FwRepPreview = KyoeiSystem.Framework.Reports.Preview;
+    using WinForms = System.Windows.Forms;
+
+    /// <summary>
+    /// 品番マスタ一括修正
+    /// </summary>
+    public partial class MST01011 : RibbonWindowViewBase
+    {
+        #region << 列挙型定義 >>
+
+        /// <summary>
+        /// データグリッドの列定義
+        /// </summary>
+        private enum GridColumnsMapping : int
+        {
+            取引先コード = 0,
+            枝番 = 1,
+            正式名称 = 2,
+            請求担当者コード = 4,
+            請求担当者 = 5,
+            支払担当者コード = 7,
+            担当者名 = 8,
+            
+        }
+
+        
+
+        #endregion
+
+        #region 画面設定項目
+        /// <summary>
+        /// ユーザ設定項目
+        /// </summary>
+        UserConfig ucfg = null;
+        CommonConfig ccfg = null;
+
+        /// <summary>
+        /// 画面固有設定項目のクラス定義
+        /// ※ 必ず public で定義する。
+        /// </summary>
+        public class ConfigMST01011 : FormConfigBase
+        {
+            public byte[] spConfig20180118 = null;
+
+        }
+
+        /// ※ 必ず public で定義する。
+        public ConfigMST01011 frmcfg = null;
+
+        public byte[] sp_Config = null;
+
+        #endregion
+
+        #region << 定数定義 >>
+
+
+
+        #endregion
+
+        #region << 変数定義 >>
+
+        private Dictionary<string, string> paramDic = new Dictionary<string, string>();
+
+        #endregion
+
+        #region バインディングプロパティ
+
+        private DataTable _searchResult;
+        public DataTable SearchResult
+        {
+            get { return _searchResult; }
+            set
+            {
+                _searchResult = value;
+                NotifyPropertyChanged();
+            }
+
+        }
+
+        #endregion
+
+        #region<< 仕入データ問合せ 初期処理群 >>
+
+        /// <summary>
+        /// 仕入データ問合せ コンストラクタ
+        /// </summary>
+        public MST01011()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        /// <summary>
+        /// 画面読み込み
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 初期状態を保存（SPREADリセット時にのみ使用する）
+            this.sp_Config = AppCommon.SaveSpConfig(this.spGridList);
+
+            base.MasterMaintenanceWindowList.Add("M72_TNT", new List<Type> { typeof(MST23010), typeof(SCHM72_TNT) });
+
+            #region 設定項目取得
+            ucfg = AppCommon.GetConfig(this);
+            ccfg = (CommonConfig)ucfg.GetConfigValue(typeof(CommonConfig));
+            frmcfg = (ConfigMST01011)ucfg.GetConfigValue(typeof(ConfigMST01011));
+            if (frmcfg == null)
+            {
+                frmcfg = new ConfigMST01011();
+                ucfg.SetConfigValue(frmcfg);
+                frmcfg.spConfig20180118 = this.sp_Config;
+            }
+            else
+            {
+                // 表示できるかチェック
+                var WidthCHK = WinForms.Screen.PrimaryScreen.Bounds.Width - frmcfg.Left;
+                if (WidthCHK > 10)
+                {
+                    this.Left = frmcfg.Left;
+                }
+                // 表示できるかチェック
+                var HeightCHK = WinForms.Screen.PrimaryScreen.Bounds.Height - frmcfg.Top;
+                if (HeightCHK > 10)
+                {
+                    this.Top = frmcfg.Top;
+                }
+                this.Height = frmcfg.Height;
+                this.Width = frmcfg.Width;
+            }
+            #endregion
+
+            spGridList.InputBindings.Add(new KeyBinding(spGridList.NavigationCommands.MoveNext, Key.Enter, ModifierKeys.None));
+
+            // コントロールの初期設定をおこなう
+            initSearchControl();
+
+            spGridList.RowCount = 0;
+
+            SetFocusToTopControl();
+            ErrorMessage = "";
+
+        }
+
+        #endregion
+
+        #region << 送信データ受信 >>
+
+        /// <summary>
+        /// 取得データの取り込み
+        /// </summary>
+        /// <param name="message"></param>
+        public override void OnReceivedResponseData(CommunicationObject message)
+        {
+            var data = message.GetResultData();
+            DataTable tbl = (data is DataTable) ? (data as DataTable) : null;
+
+            switch (message.GetMessageName())
+            {
+
+
+            }
+
+        }
+
+        /// <summary>
+        /// 受信エラー時の処理をおこなう
+        /// </summary>
+        /// <param name="message"></param>
+        public override void OnReveivedError(CommunicationObject message)
+        {
+            base.OnReveivedError(message);
+            this.Message = base.ErrorMessage;
+            base.SetFreeForInput();
+        }
+
+        #endregion
+
+        #region << リボン >>
+
+        #region F1 マスタ検索
+        /// <summary>
+        /// F1　リボン　マスタ検索
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnF1Key(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                var ctl = FocusManager.GetFocusedElement(this);
+                var m01Text = ViewBaseCommon.FindVisualParent<M01_TOK_TextBox>(ctl as UIElement);
+
+                if (m01Text == null)
+                {
+                    ViewBaseCommon.CallMasterSearch(this, this.MasterMaintenanceWindowList);
+
+                }
+                else
+                {
+                    m01Text.OpenSearchWindow(this);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                appLog.Error("検索画面起動エラー", ex);
+                this.ErrorMessage = "システムエラーです。サポートへご連絡ください。";
+
+            }
+
+        }
+        #endregion
+
+        #region F09 登録
+        /// <summary>
+        /// F9　リボン　登録
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnF9Key(object sender, KeyEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region F10 入力取消
+        /// <summary>
+        /// F10　リボン　入力取消　
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnF10Key(object sender, KeyEventArgs e)
+        {
+
+
+        }
+        #endregion
+
+        /// <summary>
+        /// F11　リボン　終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnF11Key(object sender, KeyEventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+        #region F12 削除
+        /// <summary>
+        /// F12　リボン　削除
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public override void OnF12Key(object sender, KeyEventArgs e)
+        {
+
+
+
+        }
+        #endregion
+
+        #region 一覧検索処理
+
+        /// <summary>
+        /// 検索ボタンが押下された時のイベント処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!base.CheckAllValidation())
+                {
+                    this.ErrorMessage = "入力内容に誤りがあります。";
+                    MessageBox.Show("入力内容に誤りがあります。");
+                    return;
+                }
+
+                setSearchParams();
+
+                base.SetBusyForInput();
+
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        #endregion
+
+        #region << KeyDown Events >>
+
+        /// <summary>
+        /// コントロールでキーが押下された時のイベント処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LastField_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var ctl = sender as Framework.Windows.Controls.UcLabelTwinTextBox;
+                if (ctl == null)
+                {
+                    return;
+                }
+                e.Handled = true;
+                bool chk = ctl.CheckValidation();
+                if (chk == true)
+                {
+                    Keyboard.Focus(this.btnSearch);
+                }
+                else
+                {
+                    ctl.Focus();
+                    this.ErrorMessage = ctl.GetValidationMessage();
+                }
+            }
+        }
+
+        /// <summary>
+        /// スプレッドグリッドでキーが押下された時のイベント処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void spGridList_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete && spGridList.EditElement == null)
+            {
+                e.Handled = true;
+            }
+            else if (e.Key == Key.V && (((Keyboard.GetKeyStates(Key.LeftCtrl) & KeyStates.Down) != KeyStates.Down) || ((Keyboard.GetKeyStates(Key.RightCtrl) & KeyStates.Down) != KeyStates.Down)))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        #endregion
+
+        #region Window_Closed
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            this.spGridList.InputBindings.Clear();
+            this.SearchResult = null;
+
+            if (ucfg != null)
+            {
+                if (frmcfg == null) { frmcfg = new ConfigMST01011(); }
+                frmcfg.Top = this.Top;
+                frmcfg.Left = this.Left;
+                frmcfg.Width = this.Width;
+                frmcfg.Height = this.Height;
+                frmcfg.spConfig20180118 = AppCommon.SaveSpConfig(this.spGridList);
+
+                ucfg.SetConfigValue(frmcfg);
+            }
+
+        }
+
+        #endregion
+
+
+        #region << 機能処理群 >>
+
+
+
+        #region 検索条件部の初期設定
+
+        /// <summary>
+        /// 検索条件部の初期設定をおこなう
+        /// </summary>
+        private void initSearchControl()
+        {
+
+
+        }
+
+        #endregion
+
+        #region 検索パラメータの設定
+        /// <summary>
+        /// 検索パラメータを設定する
+        /// </summary>
+        private void setSearchParams()
+        {
+
+        }
+        #endregion
+
+
+
+
+
+        #endregion
+
+    }
+
+}
