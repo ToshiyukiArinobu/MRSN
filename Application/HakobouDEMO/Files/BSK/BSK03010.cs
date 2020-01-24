6a3172d0fe20e3e813dcb0ec2190616260b29aaa
ToshiyukiArinobu <t.arinobu@kyoeisystem.co.jp>
2020-01-24 17:24:53 +0900

    本場リリース

--- Application/HakobouDEMO/Files/BSK/BSK03010.cs
+++ Application/HakobouDEMO/Files/BSK/BSK03010.cs
@@ -215,6 +215,14 @@ namespace Hakobou.Files.BSK {
                 return this.DataDefinition.ParameterFields[13];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_出力項目 {
+            get {
+                return this.DataDefinition.ParameterFields[14];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
