664dd8774629897ab8d45139457fa1fa6ce37f51
KotaroHori <51312894+KotaroHori@users.noreply.github.com>
2020-02-06 11:19:26 +0900

    ・課題No304,No329 対応 　・場所別入力画面　不要処理削除

--- Application/WCFService/Views/ZIK/ZIK04010.cs
+++ Application/WCFService/Views/ZIK/ZIK04010.cs
@@ -16,7 +16,7 @@ namespace KyoeiSystem.Application.WCFService
         /// <summary>
         /// 棚卸更新データ
         /// </summary>
-        public class StocktakingDataMember　: S10_STOCKTAKING 
+        public class StocktakingDataMember : S10_STOCKTAKING
         {
             public decimal 在庫数 { get; set; }
         }
@@ -84,7 +84,7 @@ namespace KyoeiSystem.Application.WCFService
                 string productName = pParamDic["自社品名"];
                 if (string.IsNullOrEmpty(productName) == false)
                 {
-                    stocktakingList = stocktakingList.Where(w => w.HIN.自社品名 != null && w.HIN.自社品名.Contains(productName)).ToList();
+                    stocktakingList = stocktakingList.Where(w => w.HIN.自社品名 == productName).ToList();
                 }
 
                 // 商品分類の条件チェック
@@ -170,29 +170,30 @@ namespace KyoeiSystem.Application.WCFService
                         foreach (StocktakingDataMember row in lstResult)
                         {
 
+                            // ▼課題No329 Mod Start 2020/02/05
+
                             // 処理対象チェック
                             bool bolRetTarget = CheckTargetRecord(context, row);
-                            if (bolRetTarget == false)
-                            {
-                                continue;
-                            }
 
-                            // 入出庫履歴テーブル更新チェック
-                            bool bolRetUpd = CheckS04_HISTORYUpdate(context, row);
-
-                            if (bolRetUpd == true)
+                            if (bolRetTarget == true)
                             {
+
                                 // 入出庫履歴テーブル　更新
                                 Update_S04_HISTORY(context, row);
-                            }
 
-                            // 在庫テーブル　更新
-                            Update_S03_STOK(context, row);
+                                // 在庫テーブル　更新
+                                Update_S03_STOK(context, row);
+
+                            }
 
                             // 棚卸在庫テーブル　更新
                             Update_S10_STOCKTAKING(context, row);
 
+                            // ▲課題No329 Mod End 2020/02/05
                         }
+
+                        // 棚卸在庫テーブル　更新後削除処理   課題No304 Add
+                        Delete_S10_STOCKTAKING(context, pMyCompany, pStocktakingDate, pParamDic);
 
                         // ---------------------------
                         // 終了処理
@@ -247,7 +248,7 @@ namespace KyoeiSystem.Application.WCFService
                         (x, y) => new { x.STOCKTAKING, x.HIN, SOUK = y })
                     .GroupJoin(context.S03_STOK.Where(w => w.削除日時 == null),
                             x => new { 倉庫コード = x.STOCKTAKING.倉庫コード, 品番コード = x.STOCKTAKING.品番コード, 賞味期限 = x.STOCKTAKING.賞味期限 },
-                            y => new { 倉庫コード = y.倉庫コード, 品番コード = y.品番コード,  賞味期限 = y.賞味期限},
+                            y => new { 倉庫コード = y.倉庫コード, 品番コード = y.品番コード, 賞味期限 = y.賞味期限 },
                             (x, y) => new { x, y })
                     .SelectMany(x => x.y.DefaultIfEmpty(),
                             (a, b) => new { STOCKTAKING = a.x.STOCKTAKING, HIN = a.x.HIN, SOUK = a.x.SOUK, STOK = b })
@@ -273,7 +274,7 @@ namespace KyoeiSystem.Application.WCFService
             string productName = pParamDic["自社品名"];
             if (string.IsNullOrEmpty(productName) == false)
             {
-                stocktakingList = stocktakingList.Where(w => w.HIN.自社品名 != null && w.HIN.自社品名.Contains(productName)).ToList();
+                stocktakingList = stocktakingList.Where(w => w.HIN.自社品名 == productName).ToList();
             }
 
             // 商品分類の条件チェック
@@ -331,24 +332,6 @@ namespace KyoeiSystem.Application.WCFService
             bool bolResult = true;
 
             // 処理対象チェック
-            if ((pRow.在庫数 == null || pRow.在庫数 == 0) && pRow.実在庫数 == 0)
-            {
-                bolResult = false;
-            }
-
-            return bolResult;
-        }
-
-        /// <summary>
-        /// 入出庫履歴テーブル更新チェック
-        /// </summary>
-        /// <param name="context">TRAC3Entities</param>
-        /// <param name="pRow">StocktakingDataMember</param>
-        private bool CheckS04_HISTORYUpdate(TRAC3Entities context, StocktakingDataMember pRow)
-        {
-            bool bolResult = true;
-
-            // 在庫数と実在庫数のチェック
             if (pRow.在庫数 == pRow.実在庫数)
             {
                 bolResult = false;
@@ -356,6 +339,28 @@ namespace KyoeiSystem.Application.WCFService
 
             return bolResult;
         }
+
+        // 課題No329 Del start
+
+        ///// <summary>
+        ///// 入出庫履歴テーブル更新チェック
+        ///// </summary>
+        ///// <param name="context">TRAC3Entities</param>
+        ///// <param name="pRow">StocktakingDataMember</param>
+        //private bool CheckS04_HISTORYUpdate(TRAC3Entities context, StocktakingDataMember pRow)
+        //{
+        //    bool bolResult = true;
+
+        //    // 在庫数と実在庫数のチェック
+        //    if (pRow.在庫数 == pRow.実在庫数)
+        //    {
+        //        bolResult = false;
+        //    }
+
+        //    return bolResult;
+        //}
+
+        // 課題No329 end
 
         /// <summary>
         /// 入出庫履歴テーブル　更新
@@ -414,7 +419,7 @@ namespace KyoeiSystem.Application.WCFService
             // 在庫テーブル　更新
             // ---------------------------
             S03_STOK_Update_For_Stocktaking(context, stok);
-        
+
         }
 
         /// <summary>
@@ -490,5 +495,93 @@ namespace KyoeiSystem.Application.WCFService
         }
         #endregion
 
+        #region 棚卸過去日付データ削除処理   課題No304 Add
+
+        /// <summary>
+        /// 棚卸の過去日付データ削除処理
+        /// </summary>
+        /// <param name="context">TRAC3Entities</param>
+        /// <param name="pRow">StocktakingDataMember</param>
+        private void Delete_S10_STOCKTAKING(TRAC3Entities context, int pMyCompany, string pStocktakingDate, Dictionary<string, string> pParamDic)
+        {
+
+            // ---------------------------
+            // 削除情報取得
+            // ---------------------------
+            // 棚卸在庫情報(品番マスタ,倉庫マスタ,在庫テーブル)
+
+            DateTime dteStocktaking = DateTime.Parse(pStocktakingDate);
+
+            List<StocktakingDataMember> retResult = new List<StocktakingDataMember>();
+
+            var delList =
+                context.S10_STOCKTAKING.Where(w => w.削除日時 == null && w.棚卸日 < dteStocktaking && w.更新済みFLG == 0)
+                .Join(context.M22_SOUK.Where(w => w.削除日時 == null && w.場所会社コード == pMyCompany),
+                        x => x.倉庫コード,
+                        y => y.倉庫コード,
+                        (a, b) => new { STOCKTAKING = a, SOUK = b })
+                .GroupJoin(context.M09_HIN.Where(w => w.削除日時 == null), x => x.STOCKTAKING.品番コード, y => y.品番コード, (x, y) => new { x, y })
+                .SelectMany(x => x.y.DefaultIfEmpty(), (c, d) => new { c.x.STOCKTAKING, c.x.SOUK, HIN = d })
+                .AsQueryable();
+
+            #region 入力項目による絞込
+
+            // 倉庫の条件チェック
+            string Warehouse = pParamDic["倉庫コード"];
+            if (string.IsNullOrEmpty(Warehouse) == false)
+            {
+                int iWarehouse = int.Parse(Warehouse);
+                delList = delList.Where(w => w.STOCKTAKING.倉庫コード == iWarehouse);
+            }
+
+            // 自社品番の条件チェック
+            string myProduct = pParamDic["自社品番"];
+            if (string.IsNullOrEmpty(myProduct) == false)
+            {
+                delList = delList.Where(w => w.HIN.自社品番 == myProduct);
+            }
+
+            // 品名の条件チェック
+            string productName = pParamDic["自社品名"];
+            if (string.IsNullOrEmpty(productName) == false)
+            {
+                delList = delList.Where(w => w.HIN.自社品名 == productName);
+            }
+
+            // 商品分類の条件チェック
+            int itemType;
+            if (int.TryParse(pParamDic["商品分類コード"], out itemType) == true)
+            {
+                if (itemType >= CommonConstants.商品分類.食品.GetHashCode())
+                {
+                    delList = delList.Where(w => w.HIN.商品分類 == itemType);
+                }
+            }
+
+            // ブランドの条件チェック
+            string brand = pParamDic["ブランドコード"];
+            if (string.IsNullOrEmpty(brand) == false)
+            {
+                delList = delList.Where(w => w.HIN.ブランド == brand);
+            }
+
+            // シリーズの条件チェック
+            string series = pParamDic["シリーズコード"];
+            if (string.IsNullOrEmpty(series) == false)
+            {
+                delList = delList.Where(w => w.HIN.シリーズ == series);
+            }
+            #endregion
+
+            foreach (var row in delList)
+            {
+                context.S10_STOCKTAKING.DeleteObject(row.STOCKTAKING);
+            }
+
+
+            context.SaveChanges();
+        }
+
+        #endregion
     }
 }
